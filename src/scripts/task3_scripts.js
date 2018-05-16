f3cd289986cfe3d2a3ea13a5132a4865b6c6f431
Kostiantyn Dobrutskyi <dobruzkij@gmail.com>
2018-05-16 22:38:48 +0300

    Fixed with jQuery in javascript

--- src/scripts/task3_scripts.js
+++ src/scripts/task3_scripts.js
@@ -1,164 +1,131 @@
-(function ($) {
-    randomSquare();
-    heightSquare(".dominoe-part * ", 1);
+var sliderSize = document.querySelector(".size"),
+    sliderRotateSpeed = document.querySelector(".rotate"),
+    domino = document.querySelector(".dominoe"),
+    dominoPart = document.querySelectorAll(".dominoe-part"),
+    cloneElem = document.querySelectorAll(".clone-elem"),
+    dominoPositionTop = document.querySelector(".domino-top"),
+    dominoPositionBottom = document.querySelector(".domino-bottom"),
+    btnReset = document.querySelector(".reset"),
+    rotateRight = document.getElementById("rotateRight"),
+    rotateLeft = document.getElementById("rotateLeft"),
+    object = {};
 
 
-    //---------------------------------Domino size--------------------------
-    function heightSquare(figure, point) {
-        var width = 12,
-            newWidth = width * point;
-        $(figure).css({
-            'height': newWidth + 'px',
-            'width': newWidth + 'px'
-        });
-    }
-
-    function sizeDominoe(dominoe, point) {
-
-        var height = 180,
-            newHeight = height * point,
-            width = newHeight / 2;
-        $(dominoe).css({
-            'height': newHeight + 'px',
-            'width': width + 'px'
-        });
-
-    }
-
-    $(".size").change(function () {
-        var point = $(".size").val();
-        sizeDominoe(".dominoe", point);
-        heightSquare(".dominoe-part *", point);
-    });
-
-    //------------------------------------Domino random square-----------------------------
-
-    function randomInteger(min, max) {
-        var rand = min - 0.5 + Math.random() * (max - min + 1);
-        rand = Math.round(rand);
-        return rand;
-    }
-
-    function randomSquare() {
-        switchNumber(randomInteger(0,6), ".domino-top");
-        switchNumber(randomInteger(0,6), ".domino-bottom")
-    }
-
-    function switchNumber(number, elem) {
-        switch (number) {
-            case 0 :
-                $(elem).append("<div class='dominoe-part'></div> " +
-                    "<div class='dominoe-part'></div> " +
-                    "<div class='dominoe-part'></div>");
-                break;
-
-            case 1:
-                $(elem).append("<div class='dominoe-part'></div> " +
-                    "<div class='dominoe-part'><div class='dominoe-middle'></div></div> " +
-                    "<div class='dominoe-part'></div>");
-                break;
-
-            case 2:
-                $(elem).append("<div class='dominoe-part'><div class='dominoe-right'></div></div> " +
-                    "<div class='dominoe-part'></div> " +
-                    "<div class='dominoe-part'><div class='dominoe-left'></div></div>");
-                break;
-
-            case 3:
-                $(elem).append("<div class='dominoe-part'><div class='dominoe-right'></div></div> " +
-                    "<div class='dominoe-part'><div class='dominoe-middle'></div></div> " +
-                    "<div class='dominoe-part'><div class='dominoe-left'></div></div>");
-                break;
-
-            case 4:
-                $(elem).append("<div class='dominoe-part'> <div class='dominoe-right'></div> <div class='dominoe-left'></div> </div> " +
-                    "<div class='dominoe-part'></div> " +
-                    "<div class='dominoe-part'><div class='dominoe-left'></div> <div class='dominoe-right'></div></div>");
-                break;
-
-            case 5:
-                $(elem).append("<div class='dominoe-part'> <div class='dominoe-right'></div> <div class='dominoe-left'></div> </div> " +
-                    "<div class='dominoe-part'><div class='dominoe-middle'></div></div> " +
-                    "<div class='dominoe-part'><div class='dominoe-left'></div> <div class='dominoe-right'></div></div>");
-                break;
-
-            case 6:
-                $(elem).append("<div class='dominoe-part'> <div class='dominoe-right'></div> <div class='dominoe-left'></div> </div> " +
-                    "<div class='dominoe-part'><div class='dominoe-left'></div> <div class='dominoe-right'></div></div> " +
-                    "<div class='dominoe-part'><div class='dominoe-left'></div> <div class='dominoe-right'></div></div>");
-                break;
+createObjectSquare();
+randomDominoIsDone();
+dominoPartChild(1);
 
 
+//---------------------------------Domino size--------------------------
+function dominoPartChild(point) {
+    for (var i = 0; i < dominoPart.length; i++) {
+        for (var j = 0; j < dominoPart[i].children.length; j++) {
+            heightSquare(dominoPart[i].children[j], point)
         }
     }
+}
 
-    switchNumber();
+function heightSquare(figure, point) {
+    var width = 12,
+        newWidth = width * point;
+    figure.style.height = newWidth + "px";
+    figure.style.width = newWidth + "px";
+}
+
+function sizeDomino(domino, point) {
+
+    var height = 180,
+        newHeight = height * point,
+        width = newHeight / 2;
+    domino.style.height = newHeight + "px";
+    domino.style.width = width + "px";
+
+}
+
+sliderSize.addEventListener("change", function () {
+    var point = sliderSize.value;
+    sizeDomino(domino, point);
+    dominoPartChild(point);
+});
+
+//------------------------------------Domino random square-----------------------------
 
 
-    //----------------------------------------- Rotate Dominoe---------------------------
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 
-    function rotateDominoSpeed(point) {
-        if (point > 0) {
-            $(".dominoe").css({
-                'animation-duration': 11 - point + 's'
-            })
-        } else {
-            $(".dominoe").css({
-                'animation-duration': 0 + 's'
-            })
+}
+
+function createObjectSquare() {
+    for (var i = 0; i < cloneElem[0].children.length; i++) {
+        object[i] = cloneElem[0].children[i];
+    }
+}
+
+function randomDomino(position, number) {
+    while (position.firstChild) {
+        position.removeChild(position.firstChild);
+    }
+    for (var key in object) {
+        if (+key === number) {
+            position.appendChild(object[key]);
         }
     }
+}
 
-    function rotateWay(way) {
-        var point = $(".rotate").val(1);
-        rotateDominoSpeed(1);
-        if (way === "left")
-            $(".dominoe").css({
-                'animation-name': 'rotateDominoLeft'
-            });
-        else if (way === "right") {
-            $(".dominoe").css({
-                'animation-name': 'rotateDominoRight'
-            });
-        }
-    }
-
-    function reset() {
-        $(".dominoe-half").empty();
-        randomSquare();
-        $(".rotate").val(0);
-        rotateDominoSpeed(0);
-
-        $(".size").val(1);
-        sizeDominoe(".dominoe", 1);
-        heightSquare(".dominoe-part *", 1);
-
-        $(".rotateRight").removeClass("activeBtn");
-        $(".rotateLeft").removeClass("activeBtn");
-
-    }
-
-    $(".rotateRight").click(function () {
-        rotateWay("right");
-        $(".rotateRight").addClass("activeBtn");
-        $(".rotateLeft").removeClass("activeBtn");
-    });
-
-    $(".rotateLeft").click(function () {
-        rotateWay("left");
-        $(".rotateLeft").addClass("activeBtn");
-        $(".rotateRight").removeClass("activeBtn");
-    });
-
-    $(".reset").click(function () {
-        reset();
-    });
-
-    $(".rotate").change(function () {
-        var point = $(".rotate").val();
-        rotateDominoSpeed(point);
-        console.log(point)
-    });
+function randomDominoIsDone() {
+    randomDomino(dominoPositionTop, randomInteger(0, 6));
+    randomDomino(dominoPositionBottom, randomInteger(0, 6))
+}
 
 
-})(jQuery);
+//--------------------------------------Domino Reset---------------------------------
+btnReset.addEventListener("click", function () {
+    sliderSize.value = 1;
+    sliderRotateSpeed.value = 0;
+    randomDominoIsDone();
+    rotateDominoSpeed(0);
+    dominoPartChild(1);
+    sizeDomino(domino, 1);
+    rotateRight.classList.remove("activeBtn");
+    rotateLeft.classList.remove("activeBtn");
+});
+
+
+//----------------------------------------- Rotate Dominoe---------------------------
+
+function rotateDominoSpeed(point) {
+       if(point>0){
+           domino.style.animationDuration = 11-point + "s";
+       }
+       else {
+           domino.style.animationDuration = 0 + "s";
+       }
+}
+
+
+sliderRotateSpeed.addEventListener("change", function () {
+    var point = sliderRotateSpeed.value;
+    rotateDominoSpeed(point);
+});
+
+rotateRight.addEventListener("click", function () {
+    sliderRotateSpeed.value = 1;
+    rotateDominoSpeed(1);
+    domino.classList.add("rightR");
+    domino.classList.remove("leftR");
+    rotateRight.classList.add("activeBtn");
+    rotateLeft.classList.remove("activeBtn");
+});
+
+rotateLeft.addEventListener("click", function () {
+    sliderRotateSpeed.value = 1;
+    rotateDominoSpeed(1);
+    domino.classList.remove("rightR");
+    domino.classList.add("leftR");
+    rotateRight.classList.remove("activeBtn");
+    rotateLeft.classList.add("activeBtn");
+});
+
+
+

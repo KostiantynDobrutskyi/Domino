0aadd1cc80a8f8f86bebe4378a85808973984de9
KostiantynDobrutskyi <dobruzkij@gmail.com>
2019-11-08 11:53:44 +0200

    gulpfile and package update

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,53 @@
-var gulp = require("gulp"),
+const {src, dest, watch, parallel, series} = require('gulp'),
     sync = require("browser-sync").create(),
     del = require("del"),
     plugins = require("gulp-load-plugins")({
         scope: ["devDependencies"]
     });
 
-gulp.task("html", function () {
-    return gulp.src("src/views/*.html")
+
+function html() {
+    return src("src/views/*.html")
         .pipe(plugins.htmlExtend())
-        .pipe(gulp.dest("dist"))
+        .pipe(dest("dist"))
         .pipe(sync.stream())
-});
+}
 
-gulp.task("js", function () {
-    return gulp.src("src/scripts/**/*.js")
-        .pipe(plugins.concat("app.min.js"))
-        .pipe(plugins.uglify())
-        .pipe(gulp.dest("dist/js"))
-        .pipe(sync.stream())
-});
-
-gulp.task("scripts:vendor", function () {
-    return gulp.src([
-        "node_modules/jquery/dist/jquery.min.js"
-    ])
-        .pipe(plugins.concat("scripts-vendor.min.js"))
-        .pipe(plugins.uglify())
-        .pipe(gulp.dest("dist/js"))
-});
-
-gulp.task("styles:app", function () {
-    return gulp.src("src/styles/app.less")
+function stylesApp() {
+    return src("src/styles/app.less")
         .pipe(plugins.plumber())
         .pipe(plugins.less())
         .pipe(plugins.autoprefixer())
         .pipe(plugins.rename({suffix: ".min"}))
         .pipe(plugins.cssnano())
-        .pipe(gulp.dest("dist/css"))
+        .pipe(dest("dist/css"))
         .pipe(sync.stream())
-});
+}
 
+function js() {
+    return src("src/scripts/**/*.js")
+        .pipe(plugins.concat("app.min.js"))
+        .pipe(plugins.uglify())
+        .pipe(dest("dist/js"))
+        .pipe(sync.stream())
+}
 
-
-gulp.task("clean", function (cb) {
+function clean(cb) {
     del.sync("dist");
     cb();
-});
+}
 
-gulp.task("build", ["clean"], function () {
-    gulp.start(["html", "styles:app",  "js"]);
-});
-
-gulp.task("watch", ["build"], function () {
+function watcher() {
     sync.init({
         server: "dist"
     });
-    gulp.watch("src/styles/**/*.less", ["styles:app"]);
+    watch("src/styles/**/*.less", parallel(stylesApp));
+    watch("src/views/**/*.html", parallel(html));
 
-    gulp.watch("src/scripts/*.js", ["js"]);
+}
 
-    gulp.watch("src/views/**/*.html", ["html"]);
-    // gulp.watch("dist/*.html").on("change", sync.reload());
-
-});
-
-gulp.task("default", ["watch"]);
+exports.default = series(clean, parallel(
+    html,
+    stylesApp,
+    js
+), watcher);
